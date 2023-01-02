854bf410572940344d8a7f80d4bea724b470b7ca
Aakashjha17 <80976345+Aakashjha17@users.noreply.github.com>
2023-01-02 19:33:09 +0530

    Update evaluate-reverse-polish-notation.cpp

--- day1/evaluate-reverse-polish-notation.cpp
+++ day1/evaluate-reverse-polish-notation.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(int i=0;i<tokens.size();i++){
+            if(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/" ){
+                int num1=st.top();
+                st.pop();
+                int num2=st.top();
+                st.pop();
+                int ans=0;
+                if(tokens[i]=="+"){
+                    ans=ans+(num1+num2);
+                }else if(tokens[i]=="-"){
+                    ans=ans+(num2-num1);
+                }else if(tokens[i]=="*"){
+                    ans=ans+(num1*num2);
+                }else{
+                    ans=ans+(num2/num1);
+                }
+                st.push(ans);
+            }else{
+                st.push(stoi(tokens[i]));
+            }
+        }
+        return st.top();
+    }
+};
