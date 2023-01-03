686333a99f40234639b5b498e9b84d125a0f51cc
Aakashjha17 <80976345+Aakashjha17@users.noreply.github.com>
2023-01-03 16:53:26 +0530

    Update bulls-and-cows.cpp

--- day2/bulls-and-cows.cpp
+++ day2/bulls-and-cows.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        vector<int> firstfr (10,0);
+        vector<int> secondfr (10,0);
 
+        int bulls=0,cows=0;
+        for(int i=0;i<secret.size();i++){
+            if(secret[i]==guess[i]){
+                bulls++;
+            }else{
+                firstfr[secret[i]-'0']++;
+                secondfr[guess[i]-'0']++;
+            }
+        }
+
+        for(int i=0;i<10;i++){
+            cows+=min(firstfr[i],secondfr[i]);
+        }
+        return to_string(bulls) + 'A'+ to_string(cows) + 'B';
+    }
+};
