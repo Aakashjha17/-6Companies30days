9d186206bbd044f07a62573b07c5186da354d266
Aakashjha17 <80976345+Aakashjha17@users.noreply.github.com>
2023-01-15 19:09:35 +0530

    Update query-kth-smallest-trimmed-number.cpp

--- day15/query-kth-smallest-trimmed-number.cpp
+++ day15/query-kth-smallest-trimmed-number.cpp
@@ -1,1 +1,19 @@
-
+vector<int> ans;
+        for(auto it: queries){
+            priority_queue<pair<string,int>> pq;
+            for(int i=0;i<nums.size();i++){
+                int t=nums[i].length()-it[1];
+                string p=nums[i].substr(t,it[1]);
+                if(pq.size()<it[0]){
+                    pq.push({p,i});
+                }else{
+                    if(pq.top().first>p){
+                        pq.pop();
+                        pq.push({p,i});
+                    }
+                }
+            }
+            int val=pq.top().second;
+            ans.push_back(val);
+        }
+        return ans;
